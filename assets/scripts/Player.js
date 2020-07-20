41a26fe545e8a554e4d8e843550913987736fa49
A.Madalin <antonoiumadalin@gmail.com>
2020-07-20 10:03:46 +0300

    1. Jumping up and down all the time

--- assets/scripts/Player.js
+++ assets/scripts/Player.js
@@ -15,9 +15,19 @@ cc.Class({
         acceleration : 0,
     },
 
+    setJumpAction : function(){
+        // Cocos "moveBy" API - duration, vector2 position( from 0 to jumpHeight in pixels). ease it out then
+        let jumpUp = cc.moveBy(this.jumpDuration, cc.v2(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        let jumpDown = cc.moveBy(this.jumpDuration, cc.v2(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    },
+
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {},
+    onLoad: function () {
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction); // run function setJumpAction on load, so it jumpts forever up and down
+    },
 
     start () {
 
